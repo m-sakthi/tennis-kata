8f07ceb5221a2a20c7e9610f68fb19723c542579
Sakthivel M <msv300@gmail.com>
2021-09-10 20:28:03 +0530

    Feature: Added logic to calculate player1 score

--- Tennis.js
+++ Tennis.js
@@ -1,5 +1,21 @@
 export default class Tennis {
+  constructor() {
+    this.player1Score = 0;
+  }
+
+  incrementPlayer1Score() {
+    this.player1Score += 1;
+  }
+
   currentScore() {
+    if (this.player1Score === 1) {
+      return 'fifteen love';
+    } else if (this.player1Score === 2) {
+      return 'thirty love';
+    } else if (this.player1Score === 3) {
+      return 'forty love';
+    } 
+
     return 'love all';
   }
 }
--- Tennis.test.js
+++ Tennis.test.js
@@ -10,4 +10,22 @@ describe('Tennis', () => {
   it('should return love all by default', () => {
     expect(tennisObj.currentScore()).toBe('love all');
   });
+
+  it('should return fifteen love', () => {
+    tennisObj.incrementPlayer1Score();
+    expect(tennisObj.currentScore()).toBe('fifteen love');
+  });
+
+  it('should return thirty love', () => {
+    tennisObj.incrementPlayer1Score();
+    tennisObj.incrementPlayer1Score();
+    expect(tennisObj.currentScore()).toBe('thirty love');
+  });
+
+  it('should return thirty love', () => {
+    tennisObj.incrementPlayer1Score();
+    tennisObj.incrementPlayer1Score();
+    tennisObj.incrementPlayer1Score();
+    expect(tennisObj.currentScore()).toBe('forty love');
+  });
 });
